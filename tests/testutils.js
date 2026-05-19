03e313e079ea76c6cb8941913526316a5ae75518
Elena Correa <melenabelt@gmail.com>
2026-05-19 15:54:17 -0500

    login function is not inside a test anymore because is just a helper

--- tests/testutils.js
+++ tests/testutils.js
@@ -1,11 +1,9 @@
 export async function login(page) {
-    await test.step("login", async () => {
-        await page.getByRole("textbox", { name: "Username" }).fill("standard_user");
-        await page.getByRole("textbox", { name: "Password" }).fill("secret_sauce");
-        await page.getByRole("button", { name: "Login" }).click();
+    await page.getByRole("textbox", { name: "Username" }).fill("standard_user");
+    await page.getByRole("textbox", { name: "Password" }).fill("secret_sauce");
+    await page.getByRole("button", { name: "Login" }).click();
 
-        await page.screenshot({ path: 'saucedemoportal.png', fullPage: true });
-        
-        await expect(await page.getByText("Products")).toBeVisible();
-    })
+    await page.screenshot({ path: 'saucedemoportal.png', fullPage: true });
+
+    await expect(await page.getByText("Products")).toBeVisible();
 }
